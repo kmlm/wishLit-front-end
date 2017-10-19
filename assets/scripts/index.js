60aa66d60cab170013420fb443e582e61972c618
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-19 14:18:31 -0400

    Click Handlers added for Auth

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,15 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+// API
+
+$(() => {
+  $('#new-user-form').on('submit', events.onSignUp)
+  $('#sign-in-form').on('submit', events.onSignIn)
+  $('#sign-out').on('submit', events.onSignOut)
+  $('#change-password-form').on('submit', events.onChangePassword)
+})
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
