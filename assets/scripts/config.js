5c8fff8503573922604fd6727bbb4a2410d3b4a9
kmlm <kevin.mcaleese@libertymutual.com>
2017-10-20 13:19:47 -0400

    changed config to local host for deploy
    Also added title wishLit to html

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,6 +3,7 @@
 const config = {
   apiOrigins: {
     // production: 'https://kevin-full-stack-project.herokuapp.com/',
+    production: 'http://localhost:4741/',
     development: 'http://localhost:4741/'
   }
 }
